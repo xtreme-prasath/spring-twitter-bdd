39d166e3fda21fb592e0442a496aa2e2627e4f3c
Prasath Durairaj <pair+vdurairaj@pivotal.io>
2017-01-05 11:11:24 -0500

    DR/PD Initial build - with all the IDE integration done

---
+++ src/main/java/com/example/SpringTwitterBddApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTwitterBddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTwitterBddApplication.class, args);
+	}
+}
